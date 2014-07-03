52d2a721eb6aeee2f6d4034ed350592be73dc3d3
arjungm <ajn.menon@gmail.com>
2014-07-03 14:09:21 -0700

    put all publishing and retiming boiler plate in the helper classes

--- src/playback.cpp
+++ src/playback.cpp
@@ -1,49 +1,16 @@
 #include <ros/ros.h>
 
 #include <moveit/move_group_interface/move_group.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
-#include <moveit/planning_scene/planning_scene.h>
-
-#include <moveit_msgs/DisplayRobotState.h>
-#include <moveit_msgs/DisplayTrajectory.h>
 
 #include <moveit/warehouse/planning_scene_storage.h>
-#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
-
-#include <moveit/trajectory_processing/iterative_time_parameterization.h>
 
 #include <boost/program_options.hpp>
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <std_msgs/Bool.h>
 
 #include <view_controller_msgs/CameraPlacement.h>
 
-class AnimationMonitor
-{
-  public:
-    AnimationMonitor() : last_msg_(false), status_(false) {}
-    void statusCallback(const boost::shared_ptr<std_msgs::Bool const>& status_msg)
-    {
-      if(!last_msg_ && status_msg->data)
-      {
-        status_ = true;
-      }
-      else if(last_msg_ && !status_msg->data)
-      {
-        status_ = false;
-      }
-      last_msg_ = status_msg->data;
-    }
-    bool getStatus()
-    {
-      return status_;
-    }
-  private:
-    bool last_msg_;
-    bool status_;
-};
+#include "moveit_recorder/TrajectoryRetimer.h"
+#include "moveit_recorder/AnimationRecorder.h"
 
 int main(int argc, char** argv)
 {
@@ -79,19 +46,16 @@ int main(int argc, char** argv)
 
     ROS_INFO("Connected to Warehouse DB at host (%s) and port (%d)", host.c_str(), (int)port);
 
-    ros::Publisher display_publisher = node_handle.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
-    moveit_msgs::DisplayTrajectory display_trajectory;
+    AnimationRecorder recorder( "/rviz/camera_placement",
+                                "planning_scene",
+                                "/move_group/display_planned_path",
+                                "animation_status",
+                                node_handle);
+
 
     //ask the warehouse for the scene
     std::vector<std::string> ps_names;
     pss.getPlanningSceneNames( ps_names );
-    ros::Publisher ps_pub = node_handle.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
-    while(ps_pub.getNumSubscribers() < 1)
-    {
-      ros::WallDuration sleep_t(0.5);
-      ROS_INFO("Not enough subscribers to \"%s\" topic... ", "planning_scene");
-      sleep_t.sleep();
-    }
     
     ROS_INFO("%d available scenes to display", (int)ps_names.size());
 
@@ -101,11 +65,7 @@ int main(int argc, char** argv)
       ROS_INFO("Retrieving scene %s", scene_it->c_str());
       moveit_warehouse::PlanningSceneWithMetadata pswm;
       pss.getPlanningScene(pswm, *scene_it);
-
-      // visualize the scene
-      ROS_INFO("Publishing scene...");
       moveit_msgs::PlanningScene ps_msg = static_cast<const moveit_msgs::PlanningScene&>(*pswm);
-      ps_pub.publish(ps_msg);
 
       // get query
       std::vector<std::string> pq_names;
@@ -128,26 +88,13 @@ int main(int argc, char** argv)
       
       moveit_msgs::RobotTrajectory rt_msg;
       rt_msg = static_cast<const moveit_msgs::RobotTrajectory&>(*(planning_results[ind]));
+
+      // retime it
+      TrajectoryRetimer retimer( "robot_description", mpr_msg.group_name );
+      retimer.configure(ps_msg, mpr_msg);
+      bool result = retimer.retime(rt_msg);
+      ROS_INFO("Retiming success? %s", result? "yes" : "no" );
       
-      // retime the trajectory
-      trajectory_processing::IterativeParabolicTimeParameterization traj_retimer;
-      planning_scene_monitor::PlanningSceneMonitor psm("robot_description");
-      planning_scene::PlanningScenePtr ps = psm.getPlanningScene();
-      ps->setPlanningSceneMsg( ps_msg );
-
-      robot_trajectory::RobotTrajectory rt(ps->getRobotModel(), mpr_msg.group_name);
-      moveit::core::RobotState rs(ps->getRobotModel());
-
-      rs.setVariableValues(mpr_msg.start_state.joint_state); //ref state
-      rt.setRobotTrajectoryMsg(rs, rt_msg);
-      bool success_retime = traj_retimer.computeTimeStamps(rt);
-      ROS_INFO("Retimed trajectory successfully: %s", success_retime ? "yes" : "no" );
-      rt.getRobotTrajectoryMsg(rt_msg);
-
-      // monitor
-      AnimationMonitor am;
-      ros::Subscriber sub = node_handle.subscribe("animation_status", 1, &AnimationMonitor::statusCallback, &am);
-
       // control the camera
       view_controller_msgs::CameraPlacement control_cam;
       control_cam.target_frame = "base_link";
@@ -181,47 +128,10 @@ int main(int argc, char** argv)
       control_cam.interaction_disabled = true;
       control_cam.allow_free_yaw_axis = false;
 
-      ros::Publisher camera_controller = node_handle.advertise<view_controller_msgs::CameraPlacement>("/rviz/camera_placement", 1, true);
-      while(camera_controller.getNumSubscribers() < 1)
-      {
-        ros::WallDuration sleep_t(0.5);
-        ROS_INFO("Not enough subscribers to \"%s\" topic... ", "/rviz/camera_placement");
-        sleep_t.sleep();
-      }
-      camera_controller.publish(control_cam);
-
-
-      // publish
+      // record
       if(1)
       {
-        display_trajectory.trajectory_start = mpr_msg.start_state;
-        display_trajectory.trajectory.push_back(rt_msg);
-        display_publisher.publish(display_trajectory);
-
-        // fork and record
-        pid_t pid;
-        pid = fork();
-
-        if(pid==0)
-        {
-          // child process records
-          system("recordmydesktop -o /tmp/video.ogv");
-        }
-        else
-        {
-          // parent spins while the trajectory executes and kills child
-          while(ros::ok() && !am.getStatus())
-          {
-            ros::spinOnce();
-          }
-          ROS_INFO("Animation almost ready...");
-          while(ros::ok() && am.getStatus())
-          {
-            ros::spinOnce();
-          }
-          ROS_INFO("Animation terminated");
-          kill(0,SIGINT);
-        }
+        recorder.record(control_cam, ps_msg, mpr_msg, rt_msg, "/tmp/video.ogv");
       }
     }
   }
