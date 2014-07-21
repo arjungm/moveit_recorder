782cc23a1aec16bc6016e4be5555bcedd422cb8f
arjungm <ajn.menon@gmail.com>
2014-07-21 11:04:59 -0700

    simple non blocking control input handler

--- src/cli_controller.cpp
+++ src/cli_controller.cpp
@@ -1,15 +1,17 @@
 #include <moveit_recorder/cli_controller.h>
-
-int recorder_utils::getch()
+namespace recorder_utils
 {
-  struct termios oldt, newt;
-  tcgetattr( STDIN_FILENO, &oldt);           // save old settings
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON);                 // disable buffering      
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
+  int getch()
+  {
+    struct termios oldt, newt;
+    tcgetattr( STDIN_FILENO, &oldt);           // save old settings
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON);                 // disable buffering      
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
 
-  int c = getchar();  // read character (non-blocking)
+    int c = getchar();  // read character (non-blocking)
 
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
-  return c;
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
+    return c;
+  }
 }
