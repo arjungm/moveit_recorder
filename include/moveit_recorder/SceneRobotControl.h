a320a52935f8a14c25a55b8ba78475a5938ca862
arjungm <ajn.menon@gmail.com>
2014-07-23 15:21:59 -0700

    added callback for robot end effector pose

--- include/moveit_recorder/SceneRobotControl.h
+++ include/moveit_recorder/SceneRobotControl.h
@@ -13,11 +13,13 @@ class SceneRobotControl
     SceneRobotControl(ros::NodeHandle nh, 
         const std::string& planning_scene_topic = "planning_scene",
         const std::string& from_marker_topic = "from_marker_state",
+        const std::string& from_marker_pose_topic = "from_marker_pose",
         const std::string& to_marker_topic = "to_marker_state",
         const std::string& query_save_location = "/tmp");
     ~SceneRobotControl();
     void planningSceneCallback(const boost::shared_ptr<moveit_msgs::PlanningScene const>& msg);
     void markerRobotStateCallback(const boost::shared_ptr<moveit_msgs::RobotState const>& msg);
+    void markerRobotPoseCallback(const boost::shared_ptr<geometry_msgs::Pose const>& msg);
     void getControlMessage(int dir);
     bool isSceneInitialized();
     void waitForScene();
@@ -25,25 +27,30 @@ class SceneRobotControl
 
     moveit_msgs::PlanningScene m_current_scene;
     moveit_msgs::RobotState m_current_state;
+    geometry_msgs::Pose m_current_pose;
 
     ros::Publisher m_planning_scene_publisher;
     ros::Publisher m_robot_state_publisher;
 
     ros::Subscriber m_planning_scene_subscriber;
     ros::Subscriber m_robot_state_subscriber;
+    ros::Subscriber m_robot_pose_subscriber;
 
     ros::NodeHandle m_node_handle;
 
     std::string m_planning_scene_topic;
     std::string m_from_marker_topic;
+    std::string m_from_marker_pose_topic;
     std::string m_to_marker_topic;
+
+    std::string m_query_file_location;
 
     bool m_scene_initialized;
 
     size_t m_query_num;
     std::vector< std::string > m_query_position_names;
     std::vector< std::string > m_query_6dofpose_names;
-    std::vector< double > m_query_positions;
+    std::vector< moveit_msgs::RobotState > m_query_positions;
     std::vector< double > m_query_6dofposes;
 };
 
--- src/SceneRobotControl.cpp
+++ src/SceneRobotControl.cpp
@@ -1,14 +1,19 @@
 #include "moveit_recorder/SceneRobotControl.h"
+#include <boost/filesystem.hpp>
+#include <tf/transform_datatypes.h>
+#include <tf/tf.h>
 
 SceneRobotControl::SceneRobotControl(ros::NodeHandle nh, 
     const std::string& planning_scene_topic,
     const std::string& from_marker_topic,
+    const std::string& from_marker_pose_topic,
     const std::string& to_marker_topic,
     const std::string& query_save_location)
 : m_node_handle(nh), 
   m_scene_initialized(false), 
   m_planning_scene_topic(planning_scene_topic),
   m_from_marker_topic(from_marker_topic),
+  m_from_marker_pose_topic(from_marker_pose_topic),
   m_to_marker_topic(to_marker_topic),
   m_query_num(0)
 {
@@ -18,14 +23,19 @@ SceneRobotControl::SceneRobotControl(ros::NodeHandle nh,
 
   // subscribers
   m_planning_scene_subscriber = m_node_handle.subscribe(m_planning_scene_topic, 1,
-      &SceneRobotControl::planningSceneCallback,
-      this);
+      &SceneRobotControl::planningSceneCallback, this);
   m_robot_state_subscriber = m_node_handle.subscribe(m_from_marker_topic, 1,
-      &SceneRobotControl::markerRobotStateCallback,
-      this);
+      &SceneRobotControl::markerRobotStateCallback, this);
+  m_robot_pose_subscriber = m_node_handle.subscribe(m_from_marker_pose_topic, 1,
+      &SceneRobotControl::markerRobotPoseCallback, this);
 }
 
 SceneRobotControl::~SceneRobotControl(){}
+
+void SceneRobotControl::markerRobotPoseCallback(const boost::shared_ptr<geometry_msgs::Pose const>& msg)
+{
+  m_current_pose = *msg;
+}
 
 void SceneRobotControl::planningSceneCallback(const boost::shared_ptr<moveit_msgs::PlanningScene const>& msg)
 {
@@ -51,15 +61,56 @@ void SceneRobotControl::getControlMessage(int dir)
 {
   switch(dir)
   {
+    case 'o':
+    {
+      for(int i=0; i<m_current_state.joint_state.name.size();++i)
+        ROS_INFO("[%40s] %20.2f", m_current_state.joint_state.name[i].c_str(), 
+                                m_current_state.joint_state.position[i]);
+
+      tf::Quaternion orientation;
+      tf::quaternionMsgToTF(m_current_pose.orientation, orientation);
+      double roll, pitch, yaw;
+      tf::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+
+      ROS_INFO("x:%4.2f y:%4.2f z:%4.2f r:%4.2f p:%4.2f y:%4.2f",
+                m_current_pose.position.x,
+                m_current_pose.position.y,
+                m_current_pose.position.z,
+                roll,
+                pitch,
+                yaw);
+    }
     case 's':
     {
       ROS_INFO("[Save] %d joint positions and Pose saved so far", (int)(2*(++m_query_num)));
+
+      std::stringstream start_prefix_ss;
+      start_prefix_ss << m_current_scene.name;
+      start_prefix_ss << ".start.";
+      start_prefix_ss << m_query_num;
+      m_query_position_names.push_back( start_prefix_ss.str() );
+      
+      m_query_positions.push_back(m_current_state);
+      
+      std::stringstream goal_prefix_ss;
+      goal_prefix_ss << m_current_scene.name;
+      goal_prefix_ss << ".goal.";
+      goal_prefix_ss << m_query_num;
+      m_query_6dofpose_names.push_back( goal_prefix_ss.str() );
+
       break;
     }
     case 'w':
     {
-      std::string query_file = m_current_scene.name + ".queries";
+      boost::filesystem::path query_filepath( m_query_file_location );
+      query_filepath /= m_current_scene.name;
+      std::string query_file = query_filepath.leaf().string() + ".queries";
       ROS_INFO("[Write] Writing queries to file: %s", query_file.c_str());
+      
+      // write out to file the query locations (this saves the joint positions and poses for planning)
+
+      // overwrite the warehouse scene info (this saves the robot world position)
+
       break;
     }
     case 'r':
@@ -75,6 +126,7 @@ void SceneRobotControl::getControlMessage(int dir)
       break;
     }
     default:
+      ROS_INFO("[] Unknown command");
       break;
   }
 }
