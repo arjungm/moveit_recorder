a8fc2c51f4439983835d9e8cccf3243307a5a64f
arjungm <ajn.menon@gmail.com>
2014-07-23 15:22:58 -0700

    added boost program options for topic names

--- src/BenchmarkDesigner.cpp
+++ src/BenchmarkDesigner.cpp
@@ -3,6 +3,11 @@
 #include <moveit/warehouse/planning_scene_storage.h>
 #include <moveit_recorder/SceneRobotControl.h>
 
+inline std::string get_option(const boost::program_options::variables_map& vm, const std::string& option, const std::string& default_str)
+{
+  return vm.count(option) ? vm[option].as<std::string>() : default_str;
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "designer");
@@ -18,6 +23,9 @@ int main(int argc, char** argv)
     ("host", boost::program_options::value<std::string>(), "Host for the Mongo database")
     ("port", boost::program_options::value<std::size_t>(), "Port for the Mongo database")
     ("planning_scene_topic", boost::program_options::value<std::string>(), "Planning Scene Topic for the UI")
+    ("from_marker_topic",boost::program_options::value<std::string>(), "Topic on RobotState from interactive marker")
+    ("from_marker_pose_topic",boost::program_options::value<std::string>(), "Topic on Robot Pose from interactive marker")
+    ("to_marker_topic",boost::program_options::value<std::string>(), "Topic on Robot State towards interactive marker")
     ("save_dir", boost::program_options::value<std::string>(), "Directory to store the recorded bagfile of viewpoints");
 
   boost::program_options::variables_map vm;
@@ -40,25 +48,17 @@ int main(int argc, char** argv)
 
     // planning scene connection for editing in real time
     // publish diffs to change the robot status
-    std::string planning_scene_topic = vm.count("planning_scene_topic") ? 
-                                       vm["planning_scene_topic"].as<std::string>() : 
-                                       "planning_scene";
-
-    std::string from_marker_topic = vm.count("from_marker_topic") ? 
-                                    vm["from_marker_topic"].as<std::string>() : 
-                                    "from_marker_state" ;
-    std::string to_marker_topic = vm.count("to_marker_topic") ? 
-                                  vm["to_marker_topic"].as<std::string>() : 
-                                  "to_marker_state" ;
-
-    std::string query_save_location = vm.count("save_dir") ?
-                                      vm["save_dir"].as<std::string>() :
-                                      "/tmp/";
+    std::string planning_scene_topic = get_option(vm, "planning_scene_topic", "planning_scene");
+    std::string from_marker_topic = get_option(vm, "from_marker_topic", "from_marker_state");
+    std::string from_marker_pose_topic = get_option(vm, "from_marker_pose_topic", "from_marker_pose");
+    std::string to_marker_topic = get_option(vm, "to_marker_topic", "to_marker_state");
+    std::string query_save_location = get_option(vm, "save_dir", "/tmp/");
 
     // initialize the scene and control parser
     SceneRobotControl brc(node_handle, 
                          planning_scene_topic, 
-                         from_marker_topic, 
+                         from_marker_topic,
+                         from_marker_pose_topic,
                          to_marker_topic,
                          query_save_location);
     brc.waitForScene();
