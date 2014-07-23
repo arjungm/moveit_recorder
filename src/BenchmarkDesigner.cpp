b21c3a2f370976b8b12840ce591ae5037c9ad623
arjungm <ajn.menon@gmail.com>
2014-07-23 16:46:48 -0700

    fixed bug on reading query save location

--- src/BenchmarkDesigner.cpp
+++ src/BenchmarkDesigner.cpp
@@ -15,8 +15,6 @@ int main(int argc, char** argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  sleep(20);
-
   boost::program_options::options_description desc;
   desc.add_options()
     ("help", "Show help message")
