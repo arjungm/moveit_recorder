58bc636c43504182eb6ef6974145a726c50a0d86
arjungm <ajn.menon@gmail.com>
2014-08-14 12:43:49 -0700

    migrated function from exp utils to here

--- include/moveit_recorder/utils.h
+++ include/moveit_recorder/utils.h
@@ -2,6 +2,7 @@
 #define RECORDER_UTILS_H
 
 #include <string>
+#include <view_controller_msgs/CameraPlacement.h>
 #include <boost/program_options.hpp>
 
 namespace utils
@@ -9,6 +10,10 @@ namespace utils
   inline std::string get_option(const boost::program_options::variables_map& vm, const std::string& option, const std::string& default_str)
   {
     return vm.count(option) ? vm[option].as<std::string>() : default_str;
+  }
+  namespace rosbag_storage
+  {
+    void getViewsFromBag(const std::string& bagname, std::vector<view_controller_msgs::CameraPlacement>& views);
   }
 }
 #endif
